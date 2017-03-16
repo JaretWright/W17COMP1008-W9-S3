7558211f91f40f67b86b01526d12122559b02778
JWright <JWright@10.250.11.156>
2017-03-16 12:47:38 -0400

    Added the numberButtonPushed() method

--- src/w17s3calculator/CalculatorViewController.java
+++ src/w17s3calculator/CalculatorViewController.java
@@ -7,6 +7,7 @@ package w17s3calculator;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -40,6 +41,25 @@ public class CalculatorViewController implements Initializable {
     @FXML private Label equationDisplay;
     @FXML private TextField display;
     
+    /**
+     * This method will handle updating the display
+     * when a number button is pushed
+     */
+    public void numberButtonPushed(ActionEvent event)
+    {
+        //This casts the event as a "Button" and then the
+        //getText() method returns the String from the front of Button
+        String buttonValue = ((Button)event.getSource()).getText();
+        
+        if (buttonValue.equals(".") && display.getText().contains("."))
+        {}  // ignore the button push
+        
+        //figure out if there is a value 0.  If yes, overwrite it
+        else if (display.getText().equals("0"))
+            display.setText(buttonValue);
+        else
+            display.setText(display.getText() + buttonValue);
+    }
     
     /**
      * Initializes the controller class.
